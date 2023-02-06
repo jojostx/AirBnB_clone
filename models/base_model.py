cddd9f95958430024f517b30c20604e92bf89604
Ari Jay <ikuskid7@gmail.com>
2023-02-06 14:00:42 +0100

    allow creation of baseModel instance from dict of attributes

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,20 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes/methods
     """
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         """initializes all attributes
         """
-        self.id = f'{uuid.uuid4()}'
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = f'{uuid.uuid4()}'
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if (key == "updated_at" or key == "created_at"):
+                    value = datetime.strptime(value, f)
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """returns class name, id and attribute dictionary
